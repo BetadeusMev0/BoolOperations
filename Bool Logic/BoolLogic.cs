95cb70784c70943cc5c172ac49a1beaf77172904
Home pc <vladimir.zuevg@gmail.com>
2023-11-24 21:06:03 +0700

    FINAL

--- Bool Logic/BoolLogic.cs
+++ Bool Logic/BoolLogic.cs
@@ -40,9 +40,125 @@ namespace Bool_Logic
         }
     }
 
-    
+    public static class BoolExtra 
+    {
+        
+        public static bool? BoolExpressionProcessing(string exp, Dictionary<char, bool> elems)
+        {
+            exp = new Parser(exp).postfixExpr;
+            if (exp == null || exp.Contains('(') || exp.Contains(')')) return null;
+            Stack<bool> stack = new();
+            char c = ' ';
+            for (int i = 0; i < exp.Length; i++) 
+            {
+                c = exp[i];
+                if (c == ' ') continue;
+
+
+                if (!BoolExtra.Parser.IsOperations(c) && elems.ContainsKey(c)) stack.Push(elems[c]);
+                else 
+                {
+                    if (!BoolExtra.Parser.IsOperations(c) && !elems.ContainsKey(c)) return null;
+                    if (c == '-') 
+                    {
+                        bool tmp = stack.Pop();
+                        stack.Push(BoolLogic.Not(tmp));
+                        continue;
+                    }
 
 
 
+                    if (stack.Count < 2) return null;
+                    bool second = stack.Pop();
+                    bool first = stack.Pop();
+                     
+                    stack.Push(Execute(c, first, second));
+                }
+            }
+        return stack.Pop();
+        }
+
+        private static bool Execute(char op, bool first, bool second) => op switch
+        {
+            '&' => BoolLogic.And(first, second),
+            '|' => BoolLogic.Or(first, second),
+            '↔' => BoolLogic.XNOR(first, second),
+            '←' => BoolLogic.NL(first, second),
+            '→' => BoolLogic.NM(first, second),
+            '^' => BoolLogic.XOR(first, second)
+        };
+
+
+
+
+
+
+
+        public class Parser
+        {
+            public string infixExpr { get; private set; }
+            public string? postfixExpr { get; private set; }
+            private Dictionary<char, int> operationPriority = new() {
+                {'(', 0},
+                {'↔', 1},
+                {'^', 1},
+                {'→', 2},
+                {'←', 2},
+                {'|', 2},
+                {'&', 3},
+                {'-', 4}	
+            };
+
+            
+            public Parser(string expression)
+            {
+                infixExpr = expression;
+                postfixExpr = ToPostfix(infixExpr);
+            }
+
+
+            public static bool IsOperations(char c) => c == '(' || c == '↔' || c == '→' || c == '|' || c == '&' || c == '-' || c == ')';
+
+
+
+
+
+
+            private string? ToPostfix(string infixExpr)
+            {
+                string postfixExpr = "";
+                Stack<char> stack = new();    
+                for (int i = 0; i < infixExpr.Length; i++)
+                {
+                    char c = infixExpr[i];
+                    if (!IsOperations(c))
+                    {
+                        postfixExpr += c;
+                    }
+                    else if (c == '(')
+                    {
+                        stack.Push(c);
+                    }
+                    else if (c == ')')
+                    {
+                        if (!stack.Contains('(')) return null;
+                        while (stack.Count > 0 && stack.Peek() != '(')
+                            postfixExpr += stack.Pop();
+                        stack.Pop();
+                    }
+                    else if (operationPriority.ContainsKey(c))
+                    {
+                        char op = c;
+                        while (stack.Count > 0 && (operationPriority[stack.Peek()] >= operationPriority[op]))
+                            postfixExpr += stack.Pop();
+                        stack.Push(op);
+                    }
+                }
+                foreach (char op in stack)
+                    postfixExpr += op;
+                return postfixExpr;
+            }
+        }
+    }
 
 }
--- Bool Logic/Program.cs
+++ Bool Logic/Program.cs
@@ -1,7 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using Bool_Logic;
+Dictionary<char, bool> keys = new() 
+{
+    {'x', false},
+    {'y', true},
+    {'z', false}
+};
 
-boolMethod method = BoolLogic.XNOR;
+Console.WriteLine(BoolExtra.BoolExpressionProcessing("-(x↔-y&z)", keys));
 
-Console.WriteLine(BoolDemonstration.GetSting(method)) ;
+// название обозначение обозначение_в_счётчике
+// НЕ ¬ -
+// И ∧ &
+// ИЛИ ∨ |
+// XOR ⊕ ^
+// XNOR ↔ ↔
+// Импликация → →
+// Обратная импликация ← ←
+
+
+// Источники:
+// 1. Материал к заданию (презентаця, задание, примеры)
+// 2. https://habr.com/ru/articles/596925/
+// 3. Моя фантазия
